41318501cf54e04598811452ab3609db18a17ebf
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-06-28 14:58:07 -0300

    Feat: criando o arquivo de rotas de produtos

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
