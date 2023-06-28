0460ad7354b6fd6fb86e7ecb8243f529ac252574
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-06-28 15:47:44 -0300

    Feat: adicionandofunção para  mapeamento de status na pasta utils

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,8 +1,9 @@
 const { productsService } = require('../services');
+const mapStatusHTTP = require('../utils/mapStatusHTTP');
 
 const findAllProducts = async (_req, res) => {
-  const { data } = await productsService.findAll();
-  return res.status(200).json(data);
+  const { status, data } = await productsService.findAll();
+  return res.status(mapStatusHTTP(status)).json(data);
 };
 
 module.exports = {
---
+++ backend/src/utils/mapStatusHTTP.js
@@ -0,0 +1,11 @@
+const httpStatus = {
+  SUCCESSFUL: 200,
+  CREATED: 201,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INVALID_VALUE: 422,
+};
+
+const mapStatusHTTP = (status) => httpStatus[status] || 500;
+
+module.exports = mapStatusHTTP;
